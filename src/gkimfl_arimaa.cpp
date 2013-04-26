d434bab4547a2f18394bbee50f2e61f318a9e7eb
Allen Hubbe <allenbh@gmail.com>
2013-04-26 00:16:47 -0400

    transliterate state expand code into c++

---
+++ src/gkimfl_arimaa.cpp
@@ -0,0 +1,263 @@
+
+#define NDEBUG
+#include "gkimfl_arimaa.hpp"
+
+using namespace gkimfl_arimaa;
+
+// Single trap capture function
+static inline void state_capt(
+    state& state,
+    step& step,
+    index_t pos) {
+
+  bitboard_t bit = pos_bit(pos);
+
+  if(bit & is_trap(state.bit_present)) {
+
+    color_t color;
+    rank_t rank;
+
+    color = state.get_bit_color(bit);
+
+    if(bit & bit_neighbors_all(
+          state.bit_present & state.bit_color[color])) {
+      return; // friendly piece nearby
+    }
+
+    rank = state.get_bit_rank(bit);
+
+    state.bit_present ^= bit;
+    state.bit_color[color] ^= bit;
+    state.bit_rank[rank] ^= bit;
+
+    step.capture = 1;
+    step.capture_piece = piece(color, rank);
+    step.capture_pos = pos;
+  }
+}
+
+// Transition function
+static inline void state_trans(
+    state & state,
+    step & step) {
+
+  bitboard_t bit = pos_bit(step.pos);
+  bitboard_t bit_moved = bit_neighbors(bit, step.direction);
+  bitboard_t bit_flip = bit | bit_moved;
+  bitboard_t bit_special = step.special ? bit : 0;
+
+  color_t color = piece_color(step.piece);
+  rank_t rank = piece_rank(step.piece);
+
+  if(state.bit_special) {
+    state.clear_bits(state.bit_special);
+  }
+
+  state.bit_special = bit_special;
+  state.bit_present ^= bit_flip;
+  bit_flip ^= bit_special;
+  state.bit_color[color] ^= bit_flip;
+  state.bit_rank[rank] ^= bit_flip;
+
+  if(is_trap_neighbor(bit)) {
+    state_capt(state, step, trap_pos(step.pos));
+  }
+}
+
+// ------------------------------------------------------------------
+// Arimaa Expand Functions                                           
+// ------------------------------------------------------------------
+
+// Expand steps that complete a push
+static inline void state_steps_push_complete(
+    const state& state,
+    step_list& steps) {
+
+  // get presence information
+  bitboard_t present = state.bit_present;
+  bitboard_t special = state.bit_special;
+
+  // get color information
+  color_t friend_color = state.player_color;
+  color_t enemy_color = color_opposite(friend_color);
+
+  // get pieces of each color
+  bitboard_t friend_bits = present & state.bit_color[friend_color];
+  bitboard_t enemy_bits = present & state.bit_color[enemy_color];
+
+  // get friendly bits that can be moved freely
+  friend_bits = bits_not_frozen(
+      friend_bits, enemy_bits, state.bit_rank);
+
+  // and only bits that threaten the special bit
+  friend_bits = bits_threat_to(
+      friend_bits, special, state.bit_rank);
+
+  for (int dir = 0; dir < DIRECTION_COUNT; ++dir) {
+
+    bitboard_t bit = friend_bits;
+
+    // filter in the opposite direction of the special space
+    bit &= bit_neighbors(special, dir_opposite((direction_t)dir));
+
+    steps.bits[dir].bit_mobile = bit;
+    steps.bits[dir].bit_special = 0;
+  }
+}
+
+// Expand steps when not forced to complete a push
+static inline void state_steps_normal(
+    const state& state,
+    step_list& steps) {
+
+  // get presence information
+  bitboard_t present = state.bit_present;
+  bitboard_t special = state.bit_special;
+  bitboard_t empty = ~present;
+
+  // get color information
+  color_t friend_color = state.player_color;
+  color_t enemy_color = color_opposite(friend_color);
+
+  // get pieces of each color
+  bitboard_t friend_bits = present & state.bit_color[friend_color];
+  bitboard_t enemy_bits = present & state.bit_color[enemy_color];
+
+  // get friendly bits that can be moved freely
+  bitboard_t friend_mobile_bits = bits_not_frozen(
+      friend_bits, enemy_bits, state.bit_rank);
+
+  // get enemy bits that can be moved freely
+  bitboard_t enemy_mobile_bits = bits_threatened(
+      enemy_bits, friend_bits, state.bit_rank);
+
+  // pieces that can be moved in any direction
+  bitboard_t mobile_bits = friend_mobile_bits | enemy_mobile_bits;
+
+  // move pieces in each direction
+  for(int dir = 0; dir < DIRECTION_COUNT; ++dir) {
+
+    direction_t opposite_dir = dir_opposite((direction_t)dir);
+
+    bitboard_t dir_mobile = mobile_bits;
+
+    // enemy pieces threatened by a pull in this direction
+    bitboard_t special_mobile = bits_dir_threatened(
+        enemy_bits, special, state.bit_rank, opposite_dir);
+
+    // remove friendly rabbits north or south
+    if(dir_axis((direction_t)dir) == NORTH_SOUTH &&
+        dir_sign((direction_t)dir) != friend_color) {
+      bitboard_t rabbits = state.bit_rank[RBT];
+      rabbits &= state.bit_color[friend_color];
+      dir_mobile &= ~rabbits;
+    }
+
+    // add special pieces
+    dir_mobile |= special_mobile;
+
+    // filter in the opposite direction of an empty space
+    dir_mobile &= bit_neighbors(empty, opposite_dir);
+
+    // to reduce the size of the state space,
+    // ignore non-threatening friendly special
+    bitboard_t next_special = dir_mobile & ~special_mobile;
+    bitboard_t special_friends = next_special & friend_bits;
+    bitboard_t special_enemies = next_special & enemy_bits;
+
+    special_friends &= bits_threat_to(
+      special_friends, enemy_bits, state.bit_rank);
+
+    next_special = special_friends | special_enemies;
+
+    // ready to make some moves
+    steps.bits[dir].bit_mobile = dir_mobile;
+    steps.bits[dir].bit_special = next_special;
+  }
+}
+
+// Force a push completion if special is the enemy color
+static inline bool state_force_push_complete(
+    const state& state) {
+
+  color_t enemy_color = color_opposite(state.player_color);
+
+  return ((bitboard_t)0) !=
+    (state.bit_color[enemy_color] & state.bit_special);
+}
+
+// Compute steps from a parent state
+static inline void state_steps(
+    const state& state,
+    step_list& steps) {
+
+  steps.parent = state;
+
+  if ( state_force_push_complete(state) ) {
+    state_steps_push_complete(state, steps);
+  }
+  else {
+    state_steps_normal(state, steps);
+  }
+}
+
+// Compute the next step in steps
+static inline bool steps_next_step(
+    step_list& steps,
+    step& step) {
+
+  while(steps.direction < DIRECTION_COUNT) {
+
+    step_list::bits_type & bits = steps.bits[steps.direction];
+
+    if(bits.bit_mobile) {
+      index_t pos = bit_pos(bits.bit_mobile);
+      bitboard_t bit = pos_bit(pos);
+
+      step.piece = steps.parent.get_bit_piece(bit);
+      step.pos = pos;
+      step.direction = (direction_t)steps.direction;
+
+      step.special = bit & bits.bit_special ? 1 : 0;
+
+      // capture will be set by state_capt
+      step.capture = false;
+      step.capture_piece = (piece_t)0;
+      step.capture_pos = 0;
+
+      bits.bit_mobile ^= bit;
+
+      return true;
+    }
+
+    ++steps.direction;
+  }
+
+  return false;
+}
+
+// Compute the next child state transition in steps
+static inline bool steps_next(
+    step_list& steps,
+    step& step,
+    state& child) {
+
+  if(steps_next_step(steps,step)) {
+
+    child = steps.parent;
+    state_trans(child, step); 
+
+    return true;
+  }
+
+  return false;
+}
+
+step_list::step_list(const state& parent) {
+  state_steps(parent, *this);
+}
+
+bool step_list::get_next(next_type & next) {
+  return steps_next(*this, next.first, next.second);
+}
+
---
+++ src/gkimfl_arimaa.hpp
@@ -0,0 +1,330 @@
+#ifndef gkimfl_arimaa_hpp
+#define gkimfl_arimaa_hpp
+
+#include <assert.h>
+
+#include <utility>
+
+#include <boost/cstdint.hpp>
+
+#ifdef BOOST_NO_INT64_T
+#error "This implementation requires a 64 bit integer type"
+#endif
+
+namespace gkimfl_arimaa {
+
+enum color_t { GOLD, SILV, COLOR_COUNT };
+enum rank_t { RBT, CAT, DOG, HRS, CML, ELF, RANK_COUNT };
+
+enum piece_t { GOLD_RBT, SILV_RBT, GOLD_CAT, SILV_CAT,
+  GOLD_DOG, SILV_DOG, GOLD_HRS, SILV_HRS, GOLD_CML,
+  SILV_CML, GOLD_ELF, SILV_ELF, PIECE_COUNT };
+
+enum direction_t { NORTH, SOUTH, WEST, EAST, DIRECTION_COUNT };
+enum axis_t { NORTH_SOUTH, WEST_EAST, AXIS_COUNT };
+
+typedef int index_t;
+typedef uint64_t bitboard_t;
+#define BITBOARD_C(val) UINT64_C(val)
+
+static inline color_t color_opposite(color_t color) {
+  assert(color >= 0);
+  assert(color < COLOR_COUNT);
+  return (color_t)(color ^ 1);
+}
+
+static inline piece_t piece(color_t color, rank_t rank) {
+  assert(color >= 0);
+  assert(color < COLOR_COUNT);
+  assert(rank >= 0);
+  assert(rank < RANK_COUNT);
+  return (piece_t)((rank << 1) | color);
+}
+
+static inline color_t piece_color(piece_t piece) {
+  assert(piece >= 0);
+  assert(piece < PIECE_COUNT);
+  return (color_t)(piece & 1);
+}
+
+static inline rank_t piece_rank(piece_t piece) {
+  assert(piece >= 0);
+  assert(piece < PIECE_COUNT);
+  return (rank_t)(piece >> 1);
+}
+
+static inline color_t dir_sign(direction_t dir) {
+  assert(dir >= 0);
+  assert(dir < DIRECTION_COUNT);
+  return (color_t)(dir & 1);
+}
+
+static inline axis_t dir_axis(direction_t dir) {
+  assert(dir >= 0);
+  assert(dir < DIRECTION_COUNT);
+  return (axis_t)(dir >> 1);
+}
+
+static inline direction_t dir_opposite(direction_t dir) {
+  assert(dir >= 0);
+  assert(dir < DIRECTION_COUNT);
+  return (direction_t)(dir ^ 1);
+}
+
+static inline bitboard_t has_west(bitboard_t bits) {
+  return bits & BITBOARD_C(0xfefefefefefefefe);
+}
+
+static inline bitboard_t has_east(bitboard_t bits) {
+  return bits & BITBOARD_C(0x7f7f7f7f7f7f7f7f);
+}
+
+static inline bitboard_t is_trap(bitboard_t bits) {
+  return bits & BITBOARD_C(0x0000240000240000);
+}
+
+static inline bitboard_t is_trap_neighbor(bitboard_t bits) {
+  return bits & BITBOARD_C(0x00245a24245a2400);
+}
+
+// Find nearest trap
+static inline index_t trap_pos(index_t pos) {
+  switch(pos) {
+    case 10: case 17: case 19: case 26: return 18;
+    case 13: case 20: case 22: case 29: return 21;
+    case 34: case 41: case 43: case 50: return 42;
+    case 37: case 44: case 46: case 53: return 45;
+  }
+  return 0;
+}
+
+#ifdef __GNUC__
+// ffsll declaration compatible with gcc
+extern "C" {
+__extension__ extern int ffsll (long long int __ll)
+     __THROW __attribute__ ((__const__));
+}
+
+static inline index_t bit_pos(bitboard_t bits) {
+  assert(bits != BITBOARD_C(0));
+  return ffsll(bits) - 1;
+}
+#else
+# error "you need to implement bit_pos for your compiler"
+#endif
+
+static inline bitboard_t pos_bit(index_t pos) {
+  return BITBOARD_C(1) << pos;
+}
+
+static inline bool is_exactly_one_bit(bitboard_t bit) {
+  return (bit != BITBOARD_C(0)) && (bit == pos_bit(bit_pos(bit)));
+}
+
+static inline bitboard_t bit_north(bitboard_t bits) {
+  return bits >> 8;
+}
+
+static inline bitboard_t bit_south(bitboard_t bits) {
+  return bits << 8;
+}
+
+static inline bitboard_t bit_west(bitboard_t bits) {
+  return has_west(bits) >> 1;
+}
+
+static inline bitboard_t bit_east(bitboard_t bits) {
+  return has_east(bits) << 1;
+}
+
+static inline bitboard_t bit_neighbors(bitboard_t bits, direction_t dir) {
+  switch (dir) {
+    case NORTH: return bit_north(bits);
+    case SOUTH: return bit_south(bits);
+    case WEST: return bit_west(bits);
+    case EAST: return bit_east(bits);
+  }
+  assert(false);
+  return bits;
+}
+
+static inline bitboard_t bit_neighbors_all(bitboard_t bits) {
+  return bit_north(bits) | bit_south(bits) | bit_west(bits) | bit_east(bits);
+}
+
+// Compute bits that threaten other bits
+static inline bitboard_t bits_threat_to(
+    bitboard_t bits,
+    bitboard_t threatened,
+    const bitboard_t ranks[]) {
+
+  bitboard_t rank_threatened = 0;
+  bitboard_t threats = 0;
+
+  for (int i = 0; i < RANK_COUNT; ++i) {
+    threats |= ranks[i] & bit_neighbors_all(rank_threatened);
+    rank_threatened |= ranks[i] & threatened;
+  }
+
+  return bits & threats;
+}
+
+// Compute bits threatened by other bits
+static inline bitboard_t bits_threatened(
+    bitboard_t bits,
+    bitboard_t threats,
+    const bitboard_t ranks[]) {
+
+  bitboard_t rank_threats = 0;
+  bitboard_t threatened = 0;
+
+  for (int i = RANK_COUNT - 1; 0 <= i; --i) {
+    threatened |= ranks[i] & bit_neighbors_all(rank_threats);
+    rank_threats |= ranks[i] & threats;
+  }
+
+  return bits & threatened;
+}
+
+// Compute bits threatened by other bits in one direction
+bitboard_t bits_dir_threatened(
+    bitboard_t bits,
+    bitboard_t threats,
+    const bitboard_t ranks[],
+    direction_t dir) {
+
+  bitboard_t rank_threats = 0;
+  bitboard_t threatened = 0;
+
+  for (int i = RANK_COUNT - 1; 0 <= i; --i) {
+    threatened |= ranks[i] & bit_neighbors(rank_threats, dir);
+    rank_threats |= ranks[i] & threats;
+  }
+
+  return bits & threatened;
+}
+
+// Compute bits that are not frozen by other bits
+bitboard_t bits_not_frozen(
+    bitboard_t bits,
+    bitboard_t threats,
+    const bitboard_t ranks[]) {
+
+  return bits &
+    (bit_neighbors_all(bits) |
+     ~bits_threatened(bits, threats, ranks));
+}
+
+// ------------------------------------------------------------------ //
+// Arimaa Bit Board                                                   //
+// ------------------------------------------------------------------ //
+
+struct state {
+  color_t player_color;
+  bitboard_t bit_present;
+  bitboard_t bit_special;
+  bitboard_t bit_color[COLOR_COUNT];
+  bitboard_t bit_rank[RANK_COUNT];
+
+  // Find the presence of a bit
+  bool get_bit_present(bitboard_t bit) const {
+    assert(is_exactly_one_bit(bit));
+    return bit_present & bit;
+  }
+
+  // Find the color of a bit
+  color_t get_bit_color(bitboard_t bit) const {
+    assert(is_exactly_one_bit(bit));
+    if(bit_color[GOLD] & bit) { return GOLD; }
+    assert(bit_color[SILV] & bit); return SILV;
+  }
+
+  // Find the rank of a bit
+  rank_t get_bit_rank(bitboard_t bit) const {
+    assert(is_exactly_one_bit(bit));
+    if(bit_rank[RBT] & bit) { return RBT; }
+    if(bit_rank[CAT] & bit) { return CAT; }
+    if(bit_rank[DOG] & bit) { return DOG; }
+    if(bit_rank[HRS] & bit) { return HRS; }
+    if(bit_rank[CML] & bit) { return CML; }
+    assert(bit_rank[ELF] & bit); return ELF;
+  }
+
+  // Find the piece of a bit
+  piece_t get_bit_piece(bitboard_t bit) const {
+    assert(is_exactly_one_bit(bit));
+    color_t color = get_bit_color(bit);
+    rank_t rank = get_bit_rank(bit);
+    return piece(color, rank);
+  }
+
+  // Put a piece on the board
+  void put_bit_piece(bitboard_t bit, piece_t piece) {
+    assert(is_exactly_one_bit(bit));
+    color_t color = piece_color(piece);
+    rank_t rank = piece_rank(piece);
+    bit_present |= bit;
+    bit_color[color] |= bit;
+    bit_rank[rank] |= bit;
+  }
+
+  // Clear pieces from the board
+  void clear_bits(bitboard_t bits) {
+    bits = ~bits;
+    bit_present &= bits;
+    bit_special &= bits;
+    bit_color[GOLD] &= bits;
+    bit_color[SILV] &= bits;
+    bit_rank[RBT] &= bits;
+    bit_rank[CAT] &= bits;
+    bit_rank[DOG] &= bits;
+    bit_rank[HRS] &= bits;
+    bit_rank[CML] &= bits;
+    bit_rank[ELF] &= bits;
+  }
+
+  // Put a piece on the board, clearing an already present piece
+  void set_bit_piece(bitboard_t bit, piece_t piece) {
+    if(get_bit_present(bit)) {
+      clear_bits(bit);
+    }
+    put_bit_piece(bit, piece);
+  }
+
+};
+
+// ------------------------------------------------------------------ //
+// Arimaa Transition                                                  //
+// ------------------------------------------------------------------ //
+
+// Single step transition
+struct step {
+  piece_t piece;
+  index_t pos;
+  direction_t direction;
+  bool special;
+  bool capture;
+  piece_t capture_piece;
+  index_t capture_pos;
+};
+
+// Iterable steps from a parent state
+struct step_list {
+  typedef std::pair<step, state> next_type;
+
+  struct bits_type {
+    bitboard_t bit_mobile;
+    bitboard_t bit_special;
+  };
+
+  int direction;
+  struct state parent;
+  struct bits_type bits[DIRECTION_COUNT];
+
+  step_list(const state& parent);
+  bool get_next(next_type & next);
+};
+
+} // namespace gkimfl_arimaa
+
+#endif // gkimfl_arimaa_hpp
