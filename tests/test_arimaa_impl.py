84df387535e8bb6aa17a2a261553a96542c5355b
Allen Hubbe <allenbh@gmail.com>
2013-04-28 22:59:42 -0400

    add hash tests for arimaa_impl.state.special

--- tests/test_arimaa_impl.py
+++ tests/test_arimaa_impl.py
@@ -15,12 +15,32 @@ class test_state(unittest.TestCase):
     s = arimaa_impl.state()
     self.assertEqual(hash(s), 0)
     s[4] = 4
-    val = hash(s)
     self.assertNotEqual(hash(s), 0)
+    val = hash(s)
     s[4] = 6
     self.assertNotEqual(hash(s), 0)
     self.assertNotEqual(hash(s), val)
     s[4] = None
+    self.assertEqual(hash(s), 0)
+
+  def test_special_hash(self):
+    s = arimaa_impl.state()
+    self.assertEqual(hash(s), 0)
+    s.special = (4, 4)
+    self.assertNotEqual(hash(s), 0)
+    s.special = None
+    self.assertEqual(hash(s), 0)
+
+  def test_special_replace_hash(self):
+    s = arimaa_impl.state()
+    self.assertEqual(hash(s), 0)
+    s.special = (4, 4)
+    self.assertNotEqual(hash(s), 0)
+    val = hash(s)
+    s.special = (6, 6)
+    self.assertNotEqual(hash(s), 0)
+    self.assertNotEqual(hash(s), val)
+    s.special = None
     self.assertEqual(hash(s), 0)
 
 if __name__ == '__main__':
